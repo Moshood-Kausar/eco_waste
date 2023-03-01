8d34d392932876c74c8d98d54ed7324e7ddef095
Kausar Moshood <kausarmoshood@gmail.com>
2023-03-02 00:37:41 +0100

    homepage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -11,11 +11,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
         title: Column(
-          
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
@@ -28,147 +28,141 @@ class _HomePageState extends State<HomePage> {
         automaticallyImplyLeading: false,
         toolbarHeight: 70,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 18.0, right: 18),
-        child: SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-        
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Text(
-          //   'Hi Kausar!',
-          //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-          // ),
-          // Text('Let\'s clean our community.'),
-          // SizedBox(height: 15),
-          Column(
-            children: [
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: AppColor.primary,
-                    borderRadius: BorderRadius.circular(8)),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Request Pick Up',
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 18.0, right: 18),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Text(
+                //   'Hi Kausar!',
+                //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+                // ),
+                // Text('Let\'s clean our community.'),
+                // SizedBox(height: 15),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: width,
+                      height: 200,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: 1,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Row(
+                            children: [
+                              card(
+                                  title: 'Request Pick Up',
+                                  subtitle: 'Let\'s make the environment clean',
+                                  buttontext: 'Turn On Location',
+                                  color: AppColor.primary,
+                                  txtColor: Colors.white,
+                                  buttoncolor: AppColor.primary),
+                              card(
+                                  color: AppColor.mintgreen,
+                                  title: 'Let us know about Waste Around you',
+                                  subtitle: 'Get in touch with us',
+                                  buttontext: 'Notify us',
+                                  txtColor: AppColor.primary,
+                                  buttoncolor: Colors.white),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Posts',
                         style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white),
+                            fontSize: 20, fontWeight: FontWeight.w800),
                       ),
-                      SizedBox(
-                        height: 5,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Expanded(
+                        child: posts(),
                       ),
-                      Text(
-                        'Let\'s make the environment clean',
-                        style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w200,
-                            color: Colors.white),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      MaterialButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        onPressed: () {},
-                        child: Text('Turn On Location'),
-                        color: Colors.white,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: AppColor.mintgreen,
-                    borderRadius: BorderRadius.circular(8)),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Let us know about Waste Around you',
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: AppColor.primary),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Get in touch with us',
-                        style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w200,
-                            color: AppColor.primary),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      MaterialButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        onPressed: () {},
-                        child: Text(
-                          'Notify us',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        color: AppColor.primary,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Posts',
-                  style:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-                ),
-              ),
-              // Expanded(
-
-
-                
-              //   flex: 1,
-              //   child: card()),
-             
-            ],
-          )
-        ],
-      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
+
+  Widget card(
+      {String? title,
+      subtitle,
+      buttontext,
+      buttoncolor,
+      txtColor,
+      pic,
+      Color? color}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width - 100,
+          decoration: BoxDecoration(
+              color: color, borderRadius: BorderRadius.circular(8),),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '$title',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: txtColor),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '$subtitle',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w200,
+                      color: txtColor),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  onPressed: () {},
+                  child: Text(
+                    '$buttontext',
+                    style: TextStyle(color: buttoncolor),
+                  ),
+                  color: txtColor,
+                )
+              ],
+            ),
+          )),
+    );
+  }
 }
 
-Widget card({String? title, subtitle, pic}) {
+Widget posts({String? title, subtitle, pic}) {
   return ListView.builder(
-    shrinkWrap: true,
-      itemCount: 6,
+      shrinkWrap: true,
+      itemCount: 26,
       scrollDirection: Axis.vertical,
       itemBuilder: (context, index) {
         return Padding(
--- lib/screens/trash_centres.dart
+++ lib/screens/trash_centres.dart
@@ -1,3 +1,4 @@
+import 'package:eco_waste/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class TrashCentres extends StatefulWidget {
@@ -8,17 +9,125 @@ class TrashCentres extends StatefulWidget {
 }
 
 class _TrashCentresState extends State<TrashCentres> {
+  final TextEditingController _searchController = TextEditingController();
+  String strValue = '';
+
+  bool _startloading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Nearby Trash Centres',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+            ),
+            Text('Locate the nearest trash sorting & recycle \n depot'),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+        toolbarHeight: 70,
+      ),
       body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+        child: SingleChildScrollView(
           child: Column(
-        children: const [
-          Center(
-            child: Text('Trash Centres'),
-          )
-        ],
+            children: [
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _searchController,
+                cursorColor: const Color(0xff525252),
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  fillColor: const Color(0xffF4F4F4),
+                  filled: true,
+                  isDense: true,
+                  prefixIcon: const Icon(Icons.search, color: Colors.black),
+                  contentPadding:
+                      const EdgeInsets.only(left: 10.0, right: 0.0, top: 12),
+                  hintStyle: const TextStyle(fontSize: 14, color: Colors.grey),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: AppColor.primary)),
+                  hintText: 'Search for the nearest trash centre',
+                ),
+                onChanged: (value) {
+                  strValue = value;
+                  setState(() {});
+                },
+                // textCapitalization: TextCapitalization.characters,
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Expanded(
+                    child: posts(),
+                  ),),
+            ],
+          ),
+        ),
       )),
     );
+  }
+
+  Widget posts({String? title, subtitle, pic}) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: 26,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey.withOpacity(0.08),
+                    // boxShadow: [
+                    //   BoxShadow(
+                    //     color: const Color(0xffe5e5e5).withOpacity(0.25),
+                    //     blurRadius: 14,
+                    //     offset: const Offset(0, 11),
+                    //   )
+                    // ],
+                  ),
+                  child: ListTile(
+                    isThreeLine: true,
+                    // leading: Image.asset('assets/images/boardone.png'),
+                    title: Text(
+                      'MayFair Waste Management',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    subtitle: Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: AppColor.primary,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Text(
+                              '34, Oduduwa University Estate Ife-Ile beside my mum\'s house '),
+                        ),
+                      ],
+                    ),
+                    trailing: Icon(
+                      Icons.message,
+                      color: AppColor.primary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
